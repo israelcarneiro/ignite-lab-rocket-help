6537081347c6a7dfab5086cc15270d9f4bd7576d
Israel Carneiro <isragcarneiro@gmail.com>
2022-07-23 14:28:56 -0300

    feat: should be able to see Home static interface

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/
 import { SignIn } from "./src/screens/SignIn";
 import { THEME } from "./src/styles/theme";
 import { Loading } from "./src/components/Loading";
+import { Home } from "./src/screens/Home";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -20,7 +21,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent
         />
-      { fontsLoaded ? <SignIn/> : <Loading/>}
+      { fontsLoaded ? <Home/> : <Loading/>}
     </NativeBaseProvider>
   );
 }
